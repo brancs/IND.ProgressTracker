e8969002b07d49ae6a3c769de5b7f521fd845757
Vitor Brancalião <vbrancalvv@gmail.com>
2024-10-05 19:33:05 -0300

    Setup commit

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import reactPlugin from 'eslint-plugin-react';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime'],
+  eslintConfigPrettier,
+];
